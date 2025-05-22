4c0bc51649532e63e023a581c570f33efdc84fe5
chris-mayamba <christianmayamba04@gmail.com>
2025-05-22 15:51:29 +0200

    [Add] Fonction pour recentrer la carte

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,68 +1,145 @@
 // File: app/home.js
-import React from 'react';
-import { View, Text, Button, useColorScheme, StyleSheet, Dimensions, TextInput } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
-import { useSession } from '../../ctx';
-import { Tabs } from 'expo-router';
-import Ionicons from '@expo/vector-icons/Ionicons';
-import ProfileScreen from '../profile';
+import Ionicons from "@expo/vector-icons/Ionicons";
+import * as Location from "expo-location";
+import React, { useRef, useState } from "react";
+import {
+  Dimensions,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  useColorScheme,
+  View
+} from "react-native";
+import MapView, { Marker } from "react-native-maps";
+import { useSession } from "../../ctx";
 
 export default function Home() {
+  const mapRef = useRef(null);
   const { user, logout } = useSession();
-  const isDark = useColorScheme() === 'dark';
+  const isDark = useColorScheme() === "dark";
 
   // Récupère les coordonnées GPS depuis les préférences utilisateur
   const latitude = user?.prefs?.latitude || -11.6609;
   const longitude = user?.prefs?.longitude || 27.4794;
 
-return (
-    <View style={[styles.container, { backgroundColor: isDark ? '#000' : '#fff' }]}>
+
+  // État local pour la région affichée sur la carte (latitude, longitude, et zoom via delta)
+  const [region, setRegion] = useState({
+    latitude,
+    longitude,
+    latitudeDelta: 0.05,
+    longitudeDelta: 0.05,
+  });
+
+  // Fonction asynchrone pour récupérer la position GPS actuelle
+  const getCurrentLocation = async () => {
+    const { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== "granted") {
+      alert("Permission de localisation refusée");
+      return;
+    }
+
+    const location = await Location.getCurrentPositionAsync({});
+    const { latitude, longitude } = location.coords;
+
+    const newRegion = {
+      latitude,
+      longitude,
+      latitudeDelta: 0.01,
+      longitudeDelta: 0.01,
+    };
+
+    // Mise à jour de l’état avec la nouvelle région
+    setRegion(newRegion);
+
+    // Recentrer la carte en 1 seconde
+    mapRef.current?.animateToRegion(newRegion, 1000); // 1000ms d’animation
+  };
+
+  return (
+    <View
+      style={[styles.container, { backgroundColor: isDark ? "#000" : "#fff" }]}
+    >
       {/* Barre de recherche */}
       <TextInput
         placeholder="Rechercher un lieu ou ATM"
-        placeholderTextColor={isDark ? '#ccc' : '#666'}
+        placeholderTextColor={isDark ? "#ccc" : "#666"}
         style={[
           styles.searchInput,
           {
-            backgroundColor: isDark ? '#333' : '#f0f0f0',
-            color: isDark ? '#fff' : '#000',
+            backgroundColor: isDark ? "#333" : "#f0f0f0",
+            color: isDark ? "#fff" : "#000",
           },
         ]}
       />
 
       {/* Carte avec position */}
-      <MapView
-        style={styles.map}
-        initialRegion={{
-          latitude,
-          longitude,
-          latitudeDelta: 0.05,
-          longitudeDelta: 0.05,
-        }}
-      >
+      <MapView ref={mapRef} style={styles.map} region={region}>
         <Marker
-          coordinate={{ latitude, longitude }}
+          coordinate={{
+            latitude: region.latitude,
+            longitude: region.longitude,
+          }}
           title="Vous êtes ici"
-          description={`${latitude}, ${longitude}`}
+          description={`${region.latitude}, ${region.longitude}`}
         />
       </MapView>
 
-      {/* Bouton logout */}
-      {/* <View style={styles.buttonContainer}>
-        <Button title="Se déconnecter" onPress={logout} color="#FF3B30" />
-      </View> */}
+      <View style={styles.floatingButtonContainer}>
+        <TouchableOpacity
+          style={styles.roundButton}
+          onPress={() => console.log("Afficher la list des ATM")}
+        >
+          <Ionicons name="reader" size={24} color="#fff" />
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.roundButton}
+          onPress={getCurrentLocation}
+        >
+          <Ionicons name="locate" size={24} color="#fff" />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  floatingButtonContainer: {
+    position: "absolute",
+    bottom: 20,
+    right: 20,
+    gap: 12,
+    flexDirection: "column",
+    alignItems: "center",
+  },
+
+  roundButton: {
+    width: 50,
+    height: 50,
+    borderRadius: 25, // rend le bouton circulaire
+    backgroundColor: "#007bff",
+    justifyContent: "center",
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 3,
+    elevation: 5, // pour Android
+  },
+
+  buttonText: {
+    color: "#fff",
+    fontWeight: "bold",
+  },
+
   container: {
     flex: 1,
     paddingTop: 40,
   },
   map: {
     flex: 1,
-    width: Dimensions.get('window').width,
+    width: Dimensions.get("window").width,
   },
   buttonContainer: {
     padding: 10,
--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,30 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
-  View,
-  Text,
-  Image,
-  Switch,
-  useColorScheme,
-  StyleSheet,
   Button,
-} from 'react-native';
-import { useSession } from '../../ctx'; // Pour récupérer le nom de l'utilisateur
+  FlatList,
+  SafeAreaView,
+  StyleSheet,
+  Switch,
+  Text,
+  View,
+} from "react-native";
+import { useSession } from "../../ctx";
 
 export default function ProfileScreen() {
   const [isDarkTheme, setIsDarkTheme] = useState(false);
-  const { user, logout } = useSession(); // Pour accéder à user.name et logout
+  const { user, logout } = useSession();
 
-  const toggleSwitch = () => setIsDarkTheme(prev => !prev);
+  const toggleSwitch = () => setIsDarkTheme((prev) => !prev);
 
-  const backgroundColor = isDarkTheme ? '#000' : '#fff';
-  const textColor = isDarkTheme ? '#fff' : '#000';
+  const backgroundColor = isDarkTheme ? "#000" : "#fff";
+  const textColor = isDarkTheme ? "#fff" : "#000";
+
+  const historique = [
+    { id: "1", nom: "ATM1", date: "17/05/2025", operation: "Retrait" },
+    { id: "2", nom: "ATM2", date: "15/05/2025", operation: "Dépôt" },
+  ];
 
   return (
-    <View style={[styles.container, { backgroundColor }]}>
-      <Button title="Profile" onPress={() => {}} />
+    <SafeAreaView style={[styles.container, { backgroundColor }]}>
+      <Text style={[styles.headerTitle, { color: textColor }]}>
+        Profile
+        </Text>
+
       <View style={styles.avatar} />
+
       <Text style={[styles.welcomeText, { color: textColor }]}>
-        Bonjour {user?.name || 'Invité'}
+        Bonjour {user?.name || "Invité"}
       </Text>
 
       <View style={styles.switchContainer}>
@@ -32,38 +41,105 @@ export default function ProfileScreen() {
         <Switch
           value={isDarkTheme}
           onValueChange={toggleSwitch}
-          trackColor={{ false: '#767577', true: '#81b0ff' }}
-          thumbColor={isDarkTheme ? '#f5dd4b' : '#f4f3f4'}
+          trackColor={{ false: "#767577", true: "#81b0ff" }}
+          thumbColor={isDarkTheme ? "#f5dd4b" : "#f4f3f4"}
         />
       </View>
 
-      {/* Bouton Logout en bas */}
-      <Button title="Logout" onPress={logout} color="#FF3B30" />
-    </View>
+      {/* ✅ Bouton Logout déplacé ici */}
+      <View style={styles.logoutButton}>
+        <Button title="Logout" onPress={logout} color="#FF3B30" />
+      </View>
+
+      <Text style={[styles.historiqueTitle, { color: textColor }]}>
+        Historique
+      </Text>
+
+      <View style={styles.tableHeader}>
+        <Text style={styles.tableHeaderCell}>Nom</Text>
+        <Text style={styles.tableHeaderCell}>Date</Text>
+        <Text style={styles.tableHeaderCell}>Opération</Text>
+      </View>
+
+      <FlatList
+        data={historique}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View style={styles.tableRow}>
+            <Text style={styles.tableCell}>{item.nom}</Text>
+            <Text style={styles.tableCell}>{item.date}</Text>
+            <Text style={styles.tableCell}>{item.operation}</Text>
+          </View>
+        )}
+        style={styles.table}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  headerTitle: {
+  fontSize: 24,
+  fontWeight: 'bold',
+  marginBottom: 20,
+  textTransform: 'uppercase',
+},
+
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    gap: 20,
+    alignItems: "center",
+    paddingTop: 20,
   },
   avatar: {
     width: 80,
     height: 80,
     borderRadius: 40,
     borderWidth: 2,
-    borderColor: '#aaa',
+    borderColor: "#aaa",
+    marginVertical: 20,
   },
   welcomeText: {
     fontSize: 18,
-    fontWeight: '500',
+    fontWeight: "500",
   },
   switchContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     gap: 10,
+    marginVertical: 10,
+  },
+  historiqueTitle: {
+    fontSize: 16,
+    fontWeight: "600",
+    marginTop: 20,
+    marginBottom: 10,
+  },
+  table: {
+    width: "90%",
+  },
+  tableHeader: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    backgroundColor: "#ddd",
+    paddingVertical: 8,
+    paddingHorizontal: 12,
+  },
+  tableHeaderCell: {
+    flex: 1,
+    fontWeight: "700",
+  },
+  tableRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingVertical: 6,
+    paddingHorizontal: 12,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
+  },
+  tableCell: {
+    flex: 1,
+  },
+  logoutButton: {
+    marginTop: 10,
   },
 });
